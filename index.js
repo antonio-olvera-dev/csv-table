9bd4ebcf42ebd65ad0433478c707b7376852eda8
antonioDrivel <a.olvera@drivel.app>
2021-03-01 11:19:04 +0100

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+function downloadCSV(csv, filename) {
+    let csvFile;
+    let downloadLink;
+    csvFile = new Blob([csv], { type: "text/csv" });
+    downloadLink = document.createElement("a");
+    downloadLink.download = filename;
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+}
+
+function exportTableToCSV(filename) {
+    let csv = [];
+    let rows = document.querySelectorAll("table tr");
+
+    for (let i = 0; i < rows.length; i++) {
+        let row = [];
+        let cols = rows[i].querySelectorAll("td, th");
+
+        for (let j = 0; j < cols.length; j++){
+            row.push(cols[j].innerText);
+        }
+        csv.push(row.join(","));
+    }
+    downloadCSV(csv.join("\n"), filename);
+}
+
